fcafd6a4b58bc9a59d9fcef6cca8fdfb059f0ddc
A.R. Jibon <atikurrahaman386@gmail.com>
2025-04-05 03:09:50 +0600

    Create `form-group` component with shared validation context
    - Introduced `form-group.svelte` to provide context for form field
    components.
    - Sets shared values like `form`, `name`, and a `toucherContext` to
    track user interaction.
    - Enables coordinated validation styling and messaging in nested
    components.
    - Supports generic typing for flexible form structures.

---
+++ src/lib/components/form/form-group.svelte
@@ -0,0 +1,20 @@
+<script lang="ts" generics="Form extends Record<string, unknown>">
+	import { cn } from '$lib/cn';
+	import { setContext, type Snippet } from 'svelte';
+
+	const {
+		children,
+		name,
+		form,
+		class: className
+	} = $props<{ children?: Snippet; name: keyof Form; form: Form; class?: string }>();
+
+	const toucherContext = $state({ exited: false });
+
+	setContext('exiterContext', toucherContext);
+
+	setContext('name', name);
+	setContext('form', form);
+</script>
+
+<div class={cn('space-y-2', className)}>{@render children?.()}</div>
